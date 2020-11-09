b5511510d198e4f1ee939c141917c099a4a4836f
kckc1289 <keziah.camille8@ymail.com>
2020-11-08 23:18:33 -0800

    Began implementing style for carousel and navbar:

--- sourcecode/college_bound/src/App.js
+++ sourcecode/college_bound/src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Carousel from "react-bootstrap/Carousel";
@@ -12,7 +11,7 @@ function App() {
       <head></head>
       <body>
         <div className="Nav">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar collapseOnSelect expand="lg" bg="blue" variant="blue">
             <Navbar.Brand href="#home">College Bound</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -92,43 +91,52 @@ function App() {
         <div className="Carousel">
           <Carousel>
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="college_bound/src/media/mental_health_article.png"
-                alt="First slide"
-              />
+              <a href="https://www.insidehighered.com/news/2020/09/11/students-great-need-mental-health-support-during-pandemic">
+                <img
+                  className="d-block w-100"
+                  src={"/media/campus_life_article.png"}
+                  alt="First slide"
+                />
+              </a>
               <Carousel.Caption>
-                <h3>First slide label</h3>
+                <h3>Why Campus Life Matters</h3>
+                <p>Campus life has huge rammifications for yada.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="https://thecollegepost.com/universities-us-covid-19">
+                <img
+                  className="d-block w-100"
+                  src={"/media/mental_health_article.png"}
+                  alt="Second slide"
+                />
+              </a>
+
+              <Carousel.Caption>
+                <h3>Mental Health in the Age of COVID and Zoom</h3>
                 <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  A mountain of troubling data about rising mental health
+                  problems has health advocates and providers worried about the
+                  need for additional support for struggling students and the
+                  ability of colleges to provide it.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="./media/covid_article.png"
-                alt="Second slide"
-              />
+              <a href="https://www.washingtonpost.com/national/how-black-lives-matter-born-on-the-streets-is-rising-to-power-on-campus/2015/11/17/3c113e96-8959-11e5-be8b-1ae2e4f50f76_story.html">
+                <img
+                  className="d-block w-100"
+                  src={"/media/covid_article.png"}
+                  // src="https://img.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2015/11/17/National-Enterprise/Images/University_of_Missouri_Turmoil-0302b-1394.jpg?uuid=VGBADI1nEeW69L3zc1XaDA"
+                  alt="Third slide"
+                />
+              </a>
 
               <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
+                <h3>
+                  How Collegs are Handling COVID and what that means for you
+                </h3>
+                <p>What you should be looking out for.</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
