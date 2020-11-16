f0242300d1b9d5a22ac2d3cdeb750b679f2e80b5
ameyam9 <41877888+ameyam9@users.noreply.github.com>
2020-11-15 21:24:25 -0800

    Update App.js

--- sourcecode/college_bound/src/App.js
+++ sourcecode/college_bound/src/App.js
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import NavBar from "./NavBar";
 import Carousel from "react-bootstrap/Carousel";
 
 function App() {
@@ -10,84 +8,7 @@ function App() {
     <div className="App">
       <head></head>
       <body>
-        <div className="Nav">
-          <Navbar collapseOnSelect expand="lg" bg="blue" variant="blue">
-            <Navbar.Brand href="#home">College Bound</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="nav-bar">
-                <NavDropdown
-                  title="Choosing Colleges and Degrees"
-                  id="collasible-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#action/3.1">
-                    Choosing a College
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Chosing a Degree
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    SAT vs ACT
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown
-                  title="Applying to Colleges"
-                  id="collasible-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#action/3.1">
-                    Common App
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Public Schools By State
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Specific Schools
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown
-                  title="Applying for Scholarships"
-                  id="collasible-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#action/3.1">
-                    How to Find Them
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Scholarships by School
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown
-                  title="Financing your Education"
-                  id="collasible-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#action/3.1">Loans</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">FAFSA</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Private vs Public Loans
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown
-                  title="Finding Emotional Support"
-                  id="collasible-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#action/3.1">
-                    Psychological Support
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Self-Care
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Organizations
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-
+        <NavBar />
         <div className="Carousel">
           <Carousel>
             <Carousel.Item>
