44db5557eb83135d5795ea1487c8753f22af1a88
Ameya Mellacheruvu <ameyam9@hotmail.com>
2020-11-22 21:09:58 -0800

    additional images added, css fixed

--- sourcecode/college_bound/src/pages/ApplyingToColleges.js
+++ sourcecode/college_bound/src/pages/ApplyingToColleges.js
@@ -2,7 +2,7 @@ import "../App.css";
 function ApplyingToColleges() {
   return (
     <div className="ApplyingToColleges">
-       <img className="page_header" src={"/media/choosing_a_college.png"} />
+       <img className="page_header" src={"/media/"} />
       <h1 style={{ marginTop: 50 }}>Applying to Colleges</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
 
--- sourcecode/college_bound/src/pages/PrivatevsPublicLoans.js
+++ sourcecode/college_bound/src/pages/PrivatevsPublicLoans.js
@@ -2,7 +2,7 @@ import "../App.css";
 function PrivatevsPublicLoans() {
   return (
     <div className="PrivatevsPublicLoans">
-      <img className="page_header" src={"/media/choosing_a_college.png"} />
+      <img className="page_header" src={"/media/"} />
       <h1 style={{ marginTop: 50 }}>Private vs Public Loans</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
 
