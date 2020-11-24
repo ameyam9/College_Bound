45507200dd1fdad12f42ff63183250d06600cbd0
kckc1289 <keziah.camille8@ymail.com>
2020-11-23 23:36:08 -0800

    Changed footer stylings to be sticky nav bar

--- sourcecode/college_bound/src/Footer.js
+++ sourcecode/college_bound/src/Footer.js
@@ -1,20 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { BrowserRouter as Route, Link } from "react-router-dom";
 
 function Footer() {
     return (
         <div className="Footer">
-            <h5 className="title"> College Bound </h5>
-            <ul>
-                <li className="list-unstyled">
-                    <Link to="/about"> About </Link>
-                </li>
-                <li>
-                    <a href="https://github.com/krezaey/College_Bound/tree/master/sourcecode/college_bound">Git Hub Repository</a>
-                </li>
-                <li className="list-unstyled">
-                    <a href="https://www.shutterstock.com/g/GoodStudio">Art Credit</a>
-                </li>
-            </ul>
+            <Navbar expand="lg" sticky="bottom">
+                <Navbar.Brand href="#home">College Bound</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link to="/about">About</Link>
+                        <Link href="https://github.com/krezaey/College_Bound/tree/master/sourcecode/college_bound">Git Hub Repository</Link>
+                        <Link href="https://www.shutterstock.com/g/GoodStudio">Art Credit</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 }
